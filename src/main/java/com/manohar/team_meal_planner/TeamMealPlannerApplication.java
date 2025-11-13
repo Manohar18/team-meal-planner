06d2b39479f647b55edfa6e7309f38738b368a26
manoharpulivarthi <pulivarthimanohar@gmail.com>
2025-11-13 16:53:58 -0600

    Project has successfully created and the implementation work has now begun.

---
+++ src/main/java/com/manohar/team_meal_planner/TeamMealPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.manohar.team_meal_planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamMealPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamMealPlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manohar/team_meal_planner/TeamMealPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manohar.team_meal_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamMealPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
